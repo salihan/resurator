a8f09b34256e9431356c6055af59467776ef57b5
salihan <salihan@yahoo.com>
2022-09-11 03:01:55 +0800

    3rd commit

--- cv.py
+++ cv.py
@@ -1,5 +1,6 @@
 from docx import Document
 from docx.shared import Inches, Pt
+from docx.enum.text import WD_LINE_SPACING
 
 #-------------------
 from docx.oxml import parse_xml, register_element_cls
@@ -96,6 +97,22 @@ def add_float_picture(p, image_path_or_stream, width=None, height=None, pos_x=0,
 register_element_cls('wp:anchor', CT_Anchor)
 #-------------------
 
+def make_rows_bold(*rows):
+    for row in rows:
+        for cell in row.cells:
+            for paragraph in cell.paragraphs:
+                for run in paragraph.runs:
+                    run.font.bold = True
+
+def make_rows_italic(*rows):
+    for row in rows:
+        for cell in row.cells:
+            for paragraph in cell.paragraphs:
+                for run in paragraph.runs:
+                    run.font.italic = True
+                    run.font.size = Pt(8)
+
+
 if __name__ == '__main__':
 
     name = "Mohd Salihan"
@@ -107,37 +124,84 @@ if __name__ == '__main__':
     # profile
     ## add a floating image
     p = document.add_paragraph()
-    add_float_picture(p, 'me30percent.jpg', width=Inches(1.25), pos_x=Pt(20), pos_y=Pt(20))
+    add_float_picture(p, 'me30percent.jpg', width=Inches(1.25), pos_x=Pt(40), pos_y=Pt(20))
 
     # document.add_picture('me30percent.jpg', width=Inches(1.25))
     document.add_paragraph(name + ' | ' + email + ' | ' + phone, style='Intense Quote')
 
     # some heading and para
-    p = document.add_paragraph('A plain paragraph having some ')
-    p.add_run('bold').bold = True
-    p.add_run(' and some ')
-    p.add_run('italic.').italic = True
+    document.add_heading('Profile', level=1)
+    p = document.add_paragraph('Experienced Software Developer who is open to new challenges, '
+                               'with a can-do mind-set and a strong drive to deal with any situations '
+                               'that arise in an IT environment. Fast learner who can keep up with any tool '
+                               'or methodology such as SDLC or SCRUM agile approach. Able to manage conferences, '
+                               'lead a team, do research work, and deliver tasks in a timely manner. '
+                               'Able to work effectively either in a team or independently and '
+                               'maintain professionalism under pressure.')
+    p.alignment = 3  # for left, 1 for center, 2 right, 3 justify
+    # p.add_run('bold').bold = True
+    # p.add_run(' and some ')
+    # p.add_run('italic.').italic = True
 
-    document.add_heading('Heading, level 1', level=1)
-    document.add_heading('Heading, level 2', level=2)
-    document.add_paragraph('Intense quote', style='Intense Quote')
+    # Education
+    document.add_heading('Education', level=1)
+    document.add_heading('Universiti Teknologi Petronas', level=2)
+    document.add_paragraph('Individual final year project: '
+                           'Simplifying Object-Oriented Java Programming Language', style='List Bullet')
+    document.add_paragraph('Contribution: Shorter reusable codes and a faster system performance compared'
+                           ' to the regular native codes.', style='List Bullet')
+
+    # Experiences
+    document.add_heading('Experiences', level=1)
+    document.add_heading('Software Developer | Jul 2019 – Apr 2021', level=2)
+    document.add_heading('BIT Group – Software Dept, Cyberjaya, Malaysia', level=3)
+    document.add_paragraph('Developed business intelligence module for Marine Department of Malaysia:')
+    document.add_paragraph('Liaised with customers and handled customer requests.', style='List Bullet')
+    document.add_paragraph('Assisted in data integration from multiple sources to a single reliable Marine Department of Malaysia MySQL database.', style='List Bullet')
+    document.add_paragraph('Handled and resolved data cleaning and preparation by using ETL tool Pentaho Kettle.', style='List Bullet')
+    document.add_paragraph('Updated analytical process OLAP and data warehouse deployment.', style='List Bullet')
+    document.add_paragraph('Created dashboards and reports for Marine Department of Malaysia.', style='List Bullet')
+
+    # References
+    document.add_heading('References')
+    ref1 = ['Higher College of Technology Dubai\n', 'Phone: +9712 2064772\n', 'Email: aamin@hct.ac.ae\n']
+    ref2 = ['Universiti Kebangsaan Malaysia\n', 'Phone: +6012 392 3755\n', 'Email: nazlina.ali@ukm.edu.my\n']
+    table = document.add_table(rows=1, cols=2)
+    # table.style.paragraph_format.line_spacing_rule = WD_LINE_SPACING.EXACTLY
+    # table.style.paragraph_format.space_before = None
+    # table.style.paragraph_format.space_after = None
+    # table.style.paragraph_format.line_spacing = Pt(0)
+    # commented above feature because it's not working. Will solve it later insyaallah
+    tbl_heading = table.rows[0].cells
+    tbl_heading[0].text = 'Dr. Anang Hudaya Muhamad Amin'
+    tbl_heading[1].text = 'Dr. Nazlena Mohamad Ali'
+    row_cells = table.add_row().cells
+    row_cells[0].text = ' '.join(ref1[0])
+    row_cells[1].text = ' '.join(ref2[0])
+    # row_cells[1].text = ' satu\ndua\n'
+    row_cells = table.add_row().cells
+    row_cells[0].text = ' '.join(ref1[1:])
+    row_cells[1].text = ' '.join(ref2[1:])
+    make_rows_bold(table.rows[0])
+    make_rows_italic(table.rows[1])
+
 
     # some records in a table
-    records = (
-        (3, '101', 'Spam'),
-        (7, '422', 'Eggs'),
-        (4, '631', 'Spam, spam, eggs, and spam')
-    )
-    table = document.add_table(rows=1, cols=3)
-    hd_cells = table.rows[0].cells
-    hd_cells[0].text = ' Qty '
-    hd_cells[1].text = ' id '
-    hd_cells[2].text = ' Desc '
-    for qty, id, desc in records:
-        row_cells = table.add_row().cells
-        row_cells[0].text = str(qty)
-        row_cells[1].text = id
-        row_cells[2].text = desc
+    # records = (
+    #     (3, '101', 'Spam'),
+    #     (7, '422', 'Eggs'),
+    #     (4, '631', 'Spam, spam, eggs, and spam')
+    # )
+    # table = document.add_table(rows=1, cols=3)
+    # hd_cells = table.rows[0].cells
+    # hd_cells[0].text = ' Qty '
+    # hd_cells[1].text = ' id '
+    # hd_cells[2].text = ' Desc '
+    # for qty, id, desc in records:
+    #     row_cells = table.add_row().cells
+    #     row_cells[0].text = str(qty)
+    #     row_cells[1].text = id
+    #     row_cells[2].text = desc
 
 
     document.save('cv.docx')
